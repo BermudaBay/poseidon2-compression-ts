3fdb3a0213697999e445d834d722ec666e744fcb
Yugo <yuu5infinity@gmail.com>
2025-08-13 14:33:49 +0900

    feat(hash2): expose hash_2; add test

---
+++ src/hash_2.ts
@@ -0,0 +1,5 @@
+import { permuteBn254T2 } from "./permutations/permute_t2"
+
+export function hash_2(x: bigint, y: bigint): bigint {
+  return permuteBn254T2([x, y])[0]
+}
--- tests/test.ts
+++ tests/test.ts
@@ -1,7 +1,10 @@
-import { hash_4 } from "../src/hash4"
+import { hash_4 } from "../src/hash_4"
+import { hash_2 } from "../src/hash_2"
 
-const input: [bigint, bigint, bigint, bigint] = [1n, 2n, 3n, 4n]
+const input_2: [bigint, bigint] = [1n, 2n]
+const hash_2_result = hash_2(input_2[0]!, input_2[1]!)
+console.log("hash_2_result:", hash_2_result)
 
-const hash_4_result = hash_4(input)
-
+const input_4: [bigint, bigint, bigint, bigint] = [1n, 2n, 3n, 4n]
+const hash_4_result = hash_4(input_4)
 console.log("hash_4_result:", hash_4_result)
