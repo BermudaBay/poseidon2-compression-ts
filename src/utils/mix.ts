3b9a10afc13ca4fb404ea8730b0ade040e5da398
Yugo <yuu5infinity@gmail.com>
2025-08-13 14:43:35 +0900

    feat: add t=3 external/internal mixes

--- src/utils/mix.ts
+++ src/utils/mix.ts
@@ -9,8 +9,20 @@ export const mds2_circ2_1: Mds2Fn = ([a, b]) => {
   return [s0, s1]
 }
 
+// t=2 internal mix
 export function internal_2([x, y]: [bigint, bigint]): [bigint, bigint] {
   const sum = add(x, y)
   return [add(sum, x), add(sum, mul(2n, y))]
 }
 
+// t=3 external mix
+export function external_3([a, b, c]: [bigint, bigint, bigint]): [bigint, bigint, bigint] {
+  const sum = add(add(a, b), c)
+  return [add(a, sum), add(b, sum), add(c, sum)]
+}
+
+// t=3 internal mix
+export function internal_3([a, b, c]: [bigint, bigint, bigint]): [bigint, bigint, bigint] {
+  const sum = add(add(a, b), c)
+  return [add(a, sum), add(b, sum), add(mul(2n, c), sum)]
+}
