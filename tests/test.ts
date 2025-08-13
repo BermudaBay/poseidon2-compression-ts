f6db70b1ebf0854cfb70e72684f4ba24016ce8fd
Yugo <yuu5infinity@gmail.com>
2025-08-13 14:37:39 +0900

    feat(hash1): add hash_1(x) via permuteBn254T2

--- tests/test.ts
+++ tests/test.ts
@@ -1,5 +1,10 @@
-import { hash_4 } from "../src/hash_4"
+import { hash_1 } from "../src/hash_1"
 import { hash_2 } from "../src/hash_2"
+import { hash_4 } from "../src/hash_4"
+
+const input_1: bigint = 1n
+const hash_1_result = hash_1(input_1)
+console.log("hash_1_result:", hash_1_result)
 
 const input_2: [bigint, bigint] = [1n, 2n]
 const hash_2_result = hash_2(input_2[0]!, input_2[1]!)
