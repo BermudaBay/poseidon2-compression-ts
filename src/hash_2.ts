15c56d1ad8d386c4ffafece0e3a325211da6b949
Yugo <yuu5infinity@gmail.com>
2025-08-15 17:57:47 +0900

    test: replace hash_n with permute_n in fixtures test

--- src/hash_2.ts
+++ src/hash_2.ts
@@ -3,3 +3,7 @@ import { permuteBn254T2 } from "./permutations/permute_t2"
 export function hash_2(x: bigint, y: bigint): bigint {
   return permuteBn254T2([x, y])[0]
 }
+
+export function permute_2(x: bigint, y: bigint): [bigint, bigint] {
+  return permuteBn254T2([x, y])
+}
--- src/hash_3.ts
+++ src/hash_3.ts
@@ -3,3 +3,7 @@ import { permuteBn254T3 } from "./permutations/permute_t3"
 export function hash_3(a: bigint, b: bigint, c: bigint): bigint {
   return permuteBn254T3([a, b, c])[0]
 }
+
+export function permute_3(a: bigint, b: bigint, c: bigint): [bigint, bigint, bigint] {
+  return permuteBn254T3([a, b, c])
+}
